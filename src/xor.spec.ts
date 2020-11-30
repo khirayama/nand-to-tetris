9e9d9d5eb26371e95c97024e71219f5c27a4dc65
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-01 07:27:32 +0900

    Remove needless line

--- src/xor.spec.ts
+++ src/xor.spec.ts
@@ -3,7 +3,6 @@ import { xor } from './xor';
 
 describe('Xor', function () {
   it('should return collect Binary', function () {
-    console.log(xor(0, 0), xor(0, 1), xor(1, 0), xor(1, 1));
     assert.equal(xor(0, 0), 1);
     assert.equal(xor(0, 1), 0);
     assert.equal(xor(1, 0), 0);
