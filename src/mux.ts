bed53c41c0b572e527a97da6ebd14db64a515bdb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-01 07:30:57 +0900

    Add return type to mux

--- src/mux.ts
+++ src/mux.ts
@@ -3,7 +3,7 @@ import { not } from './not';
 import { and } from './and';
 import { or } from './or';
 
-export function mux(a: Binary, b: Binary, sel: Binary) {
+export function mux(a: Binary, b: Binary, sel: Binary): Binary {
   const notsel = not(sel);
   const w1 = and(a, notsel);
   const w2 = and(b, sel);
