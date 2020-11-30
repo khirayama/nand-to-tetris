95c47616ef8180ecb2bc76e3a629f8a08e27e7a9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-01 07:38:35 +0900

    Change expression

--- src/dmux.ts
+++ src/dmux.ts
@@ -2,8 +2,7 @@ import { Binary } from './types';
 import { not } from './not';
 import { and } from './and';
 
-// export function dmux(input: Binary, sel: Binary): [Binary, Binary] {
-export function dmux(input: Binary, sel: Binary): Binary[] {
+export function dmux(input: Binary, sel: Binary): [Binary, Binary] {
   const a = and(input, not(sel));
   const b = and(input, sel);
   return [a, b];
