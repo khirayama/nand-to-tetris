d347dd1bcc701eabfda531b51aeb439965e11490
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-02 07:47:18 +0900

    Define 8-bit Bus

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,16 @@
 export type Binary = 0 | 1;
 
+export type Bus8 = [
+  Binary,
+  Binary,
+  Binary,
+  Binary,
+  Binary,
+  Binary,
+  Binary,
+  Binary,
+];
+
 export type Bus16 = [
   Binary,
   Binary,
