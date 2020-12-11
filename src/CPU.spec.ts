72b40e49a00ed8dbc5a62064122f3f78f29794d3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-12 07:06:31 +0900

    Create CPU files

---
+++ src/CPU.spec.ts
@@ -0,0 +1,8 @@
+import { assert } from 'power-assert';
+import { CPU } from './CPU';
+
+describe('CPU', function () {
+  it('should work correcty', function () {
+    console.log('TODO something');
+  });
+});
