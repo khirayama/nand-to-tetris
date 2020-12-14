635b4ff6a7deb0f18a4b503fe70a5563c7b24de5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-15 07:11:06 +0900

    Update CPU

--- src/CPU.ts
+++ src/CPU.ts
@@ -1,1 +1,13 @@
-export class CPU {}
+import { Binary, Bus16 } from './types';
+import { and } from './and';
+import { not } from './not';
+import { or } from './or';
+
+export class CPU {
+  write(inM: Bus16, instruction: Bus16, reset: Binary) {
+    const writeM = and(instruction[3], instruction[15]);
+    const loadD = and(instruction[4], instruction[15]);
+    const noti15 = not(instruction[15]);
+    const loadA = or(instruction[5], noti15);
+  }
+}
