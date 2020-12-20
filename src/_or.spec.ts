c02d347520dda81db7129d0dab0b4b9d568c2f0f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-21 07:40:40 +0900

    Rename Or file

---
+++ src/_or.spec.ts
@@ -0,0 +1,11 @@
+import assert from 'power-assert';
+import { or } from './or';
+
+describe('Or', function () {
+  it('should return correct Binary', function () {
+    assert.equal(or(0, 0), 0);
+    assert.equal(or(0, 1), 1);
+    assert.equal(or(1, 0), 1);
+    assert.equal(or(1, 1), 1);
+  });
+});
