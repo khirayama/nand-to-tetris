c883535a2dbcdb1226765602ee188b514c665f45
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 22:11:08 +0900

    Refactor mux8way16

--- src/mux8way16.ts
+++ src/mux8way16.ts
@@ -1,4 +1,4 @@
-import { Binary, Bus16 } from './types';
+import { Binary3, Word } from './types';
 import { mux16 } from './mux16';
 import { mux4way16 } from './mux4way16';
 
@@ -12,18 +12,7 @@ import { mux4way16 } from './mux4way16';
  * 110 g
  * 111 h
  */
-
-export function mux8way16(
-  a: Bus16,
-  b: Bus16,
-  c: Bus16,
-  d: Bus16,
-  e: Bus16,
-  f: Bus16,
-  g: Bus16,
-  h: Bus16,
-  sel: [Binary, Binary, Binary],
-): Bus16 {
+export function mux8way16(a: Word, b: Word, c: Word, d: Word, e: Word, f: Word, g: Word, h: Word, sel: Binary3): Word {
   const w1 = mux4way16(a, c, e, g, [sel[0], sel[1]]);
   const w2 = mux4way16(b, d, f, h, [sel[0], sel[1]]);
   return mux16(w1, w2, sel[2]);
