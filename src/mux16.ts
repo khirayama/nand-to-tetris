37a962ff4064d7802ac2db7e6de77170f288d5ab
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 20:54:19 +0900

    Refactor mux16

--- src/mux16.ts
+++ src/mux16.ts
@@ -1,10 +1,10 @@
-import { Binary, Bus16 } from './types';
+import { Binary, Word } from './types';
 import { mux } from './mux';
 
-export function mux16(a: Bus16, b: Bus16, sel: Binary): Bus16 {
+export function mux16(a: Word, b: Word, sel: Binary): Word {
   const out: Binary[] = [];
   for (let i = 0; i < a.length; i += 1) {
     out.push(mux(a[i], b[i], sel));
   }
-  return out as Bus16;
+  return out as Word;
 }
