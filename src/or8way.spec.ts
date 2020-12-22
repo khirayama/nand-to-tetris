42dc70561d5504205d286beebdd2b2ddc7a65a15
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 20:54:31 +0900

    Refactor or8way

--- src/or8way.spec.ts
+++ src/or8way.spec.ts
@@ -1,12 +1,14 @@
 import assert from 'power-assert';
+import { Binary, Binary8 } from './types';
+import { b } from './helpers';
 import { or8way } from './or8way';
 
 describe('Or8Way', function () {
   it('should return correct Binary', function () {
-    assert.equal(or8way([0, 0, 0, 0, 0, 0, 0, 0]), 0);
-    assert.equal(or8way([1, 0, 0, 0, 0, 0, 0, 0]), 1);
-    assert.equal(or8way([0, 0, 0, 1, 0, 0, 0, 0]), 1);
-    assert.equal(or8way([0, 0, 0, 0, 0, 0, 0, 1]), 1);
-    assert.equal(or8way([1, 1, 1, 1, 1, 1, 1, 1]), 1);
+    assert.equal(or8way(b<Binary8>('0000 0000')), 0);
+    assert.equal(or8way(b<Binary8>('1000 0000')), 1);
+    assert.equal(or8way(b<Binary8>('0001 0000')), 1);
+    assert.equal(or8way(b<Binary8>('0000 0001')), 1);
+    assert.equal(or8way(b<Binary8>('1111 1111')), 1);
   });
 });
--- src/or8way.ts
+++ src/or8way.ts
@@ -1,7 +1,7 @@
-import { Binary, Bus8 } from './types';
+import { Binary, Binary8 } from './types';
 import { or } from './or';
 
-export function or8way(input: Bus8): Binary {
+export function or8way(input: Binary8): Binary {
   let out: Binary = or(input[0], input[1]);
   for (let i = 2; i < input.length; i += 1) {
     out = or(out, input[i]);
