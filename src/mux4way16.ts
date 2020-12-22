184d36030480c026d99dbcae42d4e446a6b515dd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 20:59:40 +0900

    Refactor mux4way16

--- src/mux4way16.ts
+++ src/mux4way16.ts
@@ -1,7 +1,7 @@
-import { Binary, Bus16 } from './types';
+import { Binary, Binary2, Word } from './types';
 import { mux16 } from './mux16';
 
-export function mux4way16(a: Bus16, b: Bus16, c: Bus16, d: Bus16, sel: [Binary, Binary]): Bus16 {
+export function mux4way16(a: Word, b: Word, c: Word, d: Word, sel: Binary2): Word {
   const w1 = mux16(a, c, sel[0]);
   const w2 = mux16(b, d, sel[0]);
   return mux16(w1, w2, sel[1]);
