85a6cb1f25ae10c78f9f4602627567589b6e3ea6
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 20:52:56 +0900

    Fix function name or

--- src/or.ts
+++ src/or.ts
@@ -1,6 +1,6 @@
 import { Binary } from './types';
 import { nand } from './nand';
 
-export function Or(a: Binary, b: Binary): Binary {
+export function or(a: Binary, b: Binary): Binary {
   return nand(nand(a, a), nand(b, b));
 }
