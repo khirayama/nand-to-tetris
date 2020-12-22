6bf5b31e46778200f51523dd1c77d54cdcc4ece0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-22 20:54:06 +0900

    Refactor and16

--- src/and16.ts
+++ src/and16.ts
@@ -1,10 +1,10 @@
-import { Binary, Bus16 } from './types';
+import { Binary, Word } from './types';
 import { and } from './and';
 
-export function and16(a: Bus16, b: Bus16): Bus16 {
+export function and16(a: Word, b: Word): Word {
   const out: Binary[] = [];
   for (let i = 0; i < a.length; i += 1) {
     out.push(and(a[i], b[i]));
   }
-  return out as Bus16;
+  return out as Word;
 }
