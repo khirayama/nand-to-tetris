b7b1d7da0a8150ebff3b13e49fc751489499774a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-23 10:13:04 +0900

    Remove needless files and fix types

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,11 @@ export type Binary3 = [Binary, Binary, Binary];
 
 export type Binary4 = [...Binary2, ...Binary2];
 
+export type Binary6 = [...Binary4, ...Binary2];
+
 export type Binary8 = [...Binary4, ...Binary4];
+
+export type Binary9 = [...Binary6, ...Binary3];
 
 export type Binary12 = [...Binary8, ...Binary4];
 
@@ -17,3 +21,13 @@ export type Binary15 = [...Binary12, ...Binary3];
 export type Binary16 = [...Binary8, ...Binary8];
 
 export type Word = Binary16;
+
+export type Word8 = [Word, Word, Word, Word, Word, Word, Word, Word];
+
+export type Word64 = [Word8, Word8, Word8, Word8, Word8, Word8, Word8, Word8];
+
+export type Word512 = [Word64, Word64, Word64, Word64, Word64, Word64, Word64, Word64];
+
+export type Word4K = [Word512, Word512, Word512, Word512, Word512, Word512, Word512, Word512];
+
+export type Word16K = [Word4K, Word4K, Word4K, Word4K, Word4K, Word4K, Word4K, Word4K];
