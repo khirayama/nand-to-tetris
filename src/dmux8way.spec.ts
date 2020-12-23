c5eeb43b0cdb53a80f7c8b6c28e1af0ccfa512be
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-23 09:44:09 +0900

    Refactor dmux8way

--- src/dmux8way.spec.ts
+++ src/dmux8way.spec.ts
@@ -1,16 +1,18 @@
 import assert from 'power-assert';
+import { b } from './helpers';
+import { Binary8 } from './types';
 import { dmux8way } from './dmux8way';
 
 describe('DMux8Way', function () {
   it('should return correct Binary', function () {
-    assert.deepEqual(dmux8way(0, [0, 0, 0]), [0, 0, 0, 0, 0, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [0, 0, 0]), [1, 0, 0, 0, 0, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [0, 0, 1]), [0, 1, 0, 0, 0, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [0, 1, 0]), [0, 0, 1, 0, 0, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [0, 1, 1]), [0, 0, 0, 1, 0, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [1, 0, 0]), [0, 0, 0, 0, 1, 0, 0, 0]);
-    assert.deepEqual(dmux8way(1, [1, 0, 1]), [0, 0, 0, 0, 0, 1, 0, 0]);
-    assert.deepEqual(dmux8way(1, [1, 1, 0]), [0, 0, 0, 0, 0, 0, 1, 0]);
-    assert.deepEqual(dmux8way(1, [1, 1, 1]), [0, 0, 0, 0, 0, 0, 0, 1]);
+    assert.deepEqual(dmux8way(0, [0, 0, 0]), b<Binary8>('0000 0000'));
+    assert.deepEqual(dmux8way(1, [0, 0, 0]), b<Binary8>('1000 0000'));
+    assert.deepEqual(dmux8way(1, [0, 0, 1]), b<Binary8>('0100 0000'));
+    assert.deepEqual(dmux8way(1, [0, 1, 0]), b<Binary8>('0010 0000'));
+    assert.deepEqual(dmux8way(1, [0, 1, 1]), b<Binary8>('0001 0000'));
+    assert.deepEqual(dmux8way(1, [1, 0, 0]), b<Binary8>('0000 1000'));
+    assert.deepEqual(dmux8way(1, [1, 0, 1]), b<Binary8>('0000 0100'));
+    assert.deepEqual(dmux8way(1, [1, 1, 0]), b<Binary8>('0000 0010'));
+    assert.deepEqual(dmux8way(1, [1, 1, 1]), b<Binary8>('0000 0001'));
   });
 });
--- src/dmux8way.ts
+++ src/dmux8way.ts
@@ -1,4 +1,4 @@
-import { Binary, Bus8 } from './types';
+import { Binary, Binary8 } from './types';
 import { dmux } from './dmux';
 import { dmux4way } from './dmux4way';
 
@@ -13,7 +13,7 @@ import { dmux4way } from './dmux4way';
  * 111 h
  */
 
-export function dmux8way(input: Binary, sel: [Binary, Binary, Binary]): Bus8 {
+export function dmux8way(input: Binary, sel: [Binary, Binary, Binary]): Binary8 {
   const [w1, w2, w3, w4] = dmux4way(input, [sel[0], sel[1]]);
   return [...dmux(w1, sel[2]), ...dmux(w2, sel[2]), ...dmux(w3, sel[2]), ...dmux(w4, sel[2])];
 }
