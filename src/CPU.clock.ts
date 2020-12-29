64418ee23ec8dfcc5ec9d643936f4161a6a4e448
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2020-12-30 07:25:50 +0900

    Update CPU with clock

--- src/CPU.clock.ts
+++ src/CPU.clock.ts
@@ -1,5 +1,5 @@
-import { Binary, Binary2, Binary3, Binary6, Binary15, Word } from './types';
-import { zero } from './helpers';
+import { Binary, Binary3, Binary6, Binary15, Word } from './types';
+import { b, zero } from './helpers';
 import { Clock } from './Clock';
 import { Register } from './Register.clock';
 import { PC } from './PC.clock';
@@ -10,9 +10,9 @@ import { or } from './or';
 import { and } from './and';
 
 export class CPU {
-  private aRegister: Register = new Register();
+  public aRegister: Register = new Register();
 
-  private dRegister: Register = new Register();
+  public dRegister: Register = new Register();
 
   private outM: Word = zero();
 
@@ -21,10 +21,11 @@ export class CPU {
   private pc: PC = new PC();
 
   public write(clock: Clock, inM: Word, instruction: Word, reset: Binary) {
-    const [i, _xx, a, cccccc, ddd, jjj] = this.decode(instruction);
+    const [i, /* xx, */ a, cccccc, ddd, jjj] = this.decode(instruction);
+    console.log(instruction.join(''));
+    console.log(i, a, cccccc, ddd, jjj);
 
-    const tmpClock = clock.get() === 0 ? new Clock() : new Clock().next();
-    tmpClock.next();
+    const tmpClock = clock.get() === 0 ? new Clock().next() : new Clock();
     const [aluout, zr, ng] = alu(
       this.dRegister.read(tmpClock),
       mux16(this.aRegister.read(tmpClock), inM, a),
@@ -44,13 +45,15 @@ export class CPU {
     const jumpFlag = or(or(and(jjj[0], ng), and(jjj[1], zr)), and(jjj[2], ps));
     this.pc.write(clock, this.aRegister.read(tmpClock), 1, and(jumpFlag, i), reset);
 
+    console.log(instruction.join(''), aluout.join(''), i);
     this.aRegister.write(clock, mux16(instruction, aluout, i), or(not(i), ddd[0]));
 
     const writeM = and(ddd[2], i);
     const writeDest0 = this.aRegister.read(clock);
-    const writeDest1 = writeDest0;
+    const writeDest1 = writeDest0.concat() as Word;
     writeDest1[0] = writeM;
     // NOTE 条件分岐いるかも?
+    console.log(writeDest0.join(''), writeDest1.join(''), i);
     this.writeDst = mux16(writeDest0, writeDest1, i);
     // NOTE ここ間違ってるかも
     const newClock = new Clock();
@@ -67,10 +70,10 @@ export class CPU {
     return [this.outM, writeM, addressM, count];
   }
 
-  private decode(instruction: Word): [Binary, Binary2, Binary, Binary6, Binary3, Binary3] {
+  private decode(instruction: Word): [Binary, /* Binary2, */ Binary, Binary6, Binary3, Binary3] {
     return [
       instruction[0],
-      [instruction[1], instruction[2]],
+      // [instruction[1], instruction[2]],
       instruction[3],
       [instruction[4], instruction[5], instruction[6], instruction[7], instruction[8], instruction[9]],
       [instruction[10], instruction[11], instruction[12]],
