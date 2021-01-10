5ea98cac760ea450f7c50bba8fd5a4a02274af30
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-10 14:46:27 +0900

    Define get_filename

--- software/hackasm/src/main.rs
+++ software/hackasm/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world");
+    let filename = get_filename();
+    println!("{}", filename);
+}
+
+fn get_filename() -> String {
+    let args: Vec<String> = env::args().collect();
+    let name = args.get(1).unwrap_or_else(|| {
+        println!("Require arguments.");
+        process::exit(1);
+    });
+    return String::from(name);
 }
