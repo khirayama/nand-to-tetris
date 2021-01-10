1bd7210a79b0b33d70828dc799b0fb1cc7cfa1e2
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-01-10 16:06:31 +0900

    Define Symbols

--- software/hackasm/src/symbols.rs
+++ software/hackasm/src/symbols.rs
@@ -19,3 +19,46 @@ pub struct Symbols {
     table: HashMap<String, Symbol>,
     offset: usize,
 }
+
+const DEFINED_SYMBOLS: &[(&str, usize)] = &[
+    ("SP", 0),
+    ("LCL", 1),
+    ("ARG", 2),
+    ("THIS", 3),
+    ("THAT", 4),
+    ("SCREEN", 16384),
+    ("KBD", 24576),
+];
+
+impl Symbols {
+    pub fn new() -> Symbols {
+        let mut table: HashMap<String, Symbol> = HashMap::new();
+
+        DEFINED_SYMBOLS.into_iter().for_each(|(name, addr)| {
+            let name = String::from(*name);
+            table.insert(
+                name.clone(),
+                Symbol {
+                    name,
+                    addr: *addr,
+                },
+            );
+        });
+
+        for n in 0..16 {
+            let name = format!("R{}", n);
+            table.insert(
+                name.clone(),
+                Symbol {
+                    name,
+                    addr: n,
+                },
+            );
+        }
+
+        Symbols {
+            table,
+            offset: 16,
+        }
+    }
+}
