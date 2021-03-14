d7c3d0d0af30ffc0d61b4e39f39d249d766ac1a1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-15 07:30:34 +0900

    Rewrite binary in PC

--- hardware/src/hardware/PC.ts
+++ hardware/src/hardware/PC.ts
@@ -1,4 +1,5 @@
 import { Binary, Word } from './types';
+import { b } from './helpers';
 import { Register } from './Register';
 import { inc16 } from './inc16';
 import { mux16 } from './mux16';
@@ -11,7 +12,7 @@ export class PC {
     const w1 = inc16(fb);
     const w2 = mux16(fb, w1, inc);
     const w3 = mux16(w2, input, load);
-    const w4 = mux16(w3, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], reset);
+    const w4 = mux16(w3, b('0000 0000 0000 0000'), reset);
     return this.register.write(w4, 1);
   }
 
