57ffb481b00501bca11e9eb54598e0cdb3f3adf9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-16 07:13:06 +0900

    Implement binary to string function named b2s

--- hardware/src/hardware/helpers.ts
+++ hardware/src/hardware/helpers.ts
@@ -1,3 +1,5 @@
+import { Binary } from './types';
+
 export function zero<T>(len: number = 16): T {
   const result = [];
   for (let i = 0; i < len; i += 1) {
@@ -11,4 +13,16 @@ export function b<T>(str: string): T {
     .replace(/ /g, '')
     .split('')
     .map((n) => Number(n)) as unknown) as T;
+}
+
+export function b2s(b: Binary[]): string {
+  let result = '';
+  for (let i = 0; i < b.length; i += 1) {
+    if (i % 4 === 0 && i !== 0) {
+      result += ' ';
+    }
+    result += String(b[i]);
+  }
+
+  return result;
 }
