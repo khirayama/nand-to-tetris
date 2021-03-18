575e434236c72877db6e31337af748840cf21670
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-18 22:05:32 +0900

    Add Screen to Memory

--- hardware/src/hardware/Memory.ts
+++ hardware/src/hardware/Memory.ts
@@ -11,23 +11,18 @@ export class Memory {
   private screen: Screen = new Screen();
 
   public write(input: Binary16, load: Binary, address: Binary15) {
-    const [loadram0, loadram1, loadscreen] = dmux4way(load, [address[13], address[14]]);
+    const [loadram0, loadram1, loadscreen] = dmux4way(load, [address[0], address[1]]);
     const loadram = or(loadram0, loadram1);
     const ramout = this.ram.write(input, loadram, address);
-    // const scrout = this.screen.write(input, loadscreen, address.slice(0, 14) as Binary13);
-    this.screen.write(input, loadscreen, address.slice(0, 14) as Binary13);
-    // keyboard
+    this.screen.write(input, loadscreen, address.slice(2) as Binary13);
+    // TODO Keyboard
     return mux4way16(
       ramout,
       ramout,
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [address[13], address[14]],
+      [address[0], address[1]],
     );
-  }
-
-  public display() {
-    this.screen.display();
   }
 
   public read(address: Binary15) {
