ef9deb797a30e7c92b6597d78d80f1ee7a2ddf4f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-20 08:32:39 +0900

    Create resetStyles

---
+++ hardware/src/components/resetStyles.ts
@@ -0,0 +1,108 @@
+import { css } from 'styled-jsx/css';
+
+export const resetStyles = css.global`
+  html,
+  body,
+  section,
+  header,
+  footer,
+  div,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a,
+  span,
+  ul,
+  li,
+  label,
+  time,
+  input,
+  textarea,
+  form,
+  nav,
+  hr,
+  code,
+  select,
+  button,
+  table,
+  thead,
+  tbody,
+  tr,
+  th,
+  td,
+  small,
+  img,
+  i {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    border-radius: 0;
+    font-weight: normal;
+    font-style: normal;
+    font-family: inherit;
+    vertical-align: baseline;
+    -webkit-appearance: none;
+  }
+
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  nav,
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  table,
+  th,
+  td {
+    border: none;
+  }
+
+  img {
+    vertical-align: top;
+    height: auto;
+  }
+
+  input,
+  textarea,
+  button {
+    font-size: inherit;
+    color: inherit;
+  }
+
+  /* Applicattion */
+  html {
+    font-family: sans-serif;
+    font-size: 18px;
+    text-align: justify;
+    color: #222222;
+  }
+`;
