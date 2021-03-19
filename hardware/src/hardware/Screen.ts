0f0a59e2cb4e794a3ac8ac2fc268b0081f74230d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-20 08:33:26 +0900

    Fix bugs in Screen

--- hardware/src/hardware/Screen.ts
+++ hardware/src/hardware/Screen.ts
@@ -1,7 +1,7 @@
-import { Binary, Binary16, Binary13, Binary12 } from './types';
+import { Binary, Binary16, Binary13, Binary12, Word } from './types';
 import { RAM4K } from './RAM4K';
 import { dmux } from './dmux';
-import { zero } from './helpers';
+import { b, zero } from './helpers';
 
 export class Screen {
   private rams: [RAM4K, RAM4K] = [new RAM4K(), new RAM4K()];
@@ -12,21 +12,30 @@ export class Screen {
     this.rams[0].write(input, bits[0], addrss);
     this.rams[1].write(input, bits[1], addrss);
 
-    this.display();
-
     return /* TODO */ zero();
   }
 
-  private display() {
-    const pixels = this.rams[0].read().concat(this.rams[1].read());
-    let screen = '';
-    for (let i = 0; i < pixels.length; i += 1) {
-      if (i !== 0 && i % 512 === 0) {
-        screen += '\n';
+  public display() {
+    let len = 12;
+    const result = [];
+    for (const ram of this.rams) {
+      for (let i = 0; i < Math.pow(2, len); i += 1) {
+        const bi = (zero<number[]>(len).join('') + i.toString(2)).slice(len * -1);
+        const val = ram.read(b(bi));
+        result.push(val);
       }
-      const pixel = pixels[i];
-      screen += pixel ? 'X' : ' ';
     }
-    return screen;
+    return result;
+
+    // const pixels = this.rams[0].read().concat(this.rams[1].read());
+    // let screen = '';
+    // for (let i = 0; i < pixels.length; i += 1) {
+    //   if (i !== 0 && i % 512 === 0) {
+    //     screen += '\n';
+    //   }
+    //   const pixel = pixels[i];
+    //   screen += pixel ? 'X' : ' ';
+    // }
+    // return screen;
   }
 }
