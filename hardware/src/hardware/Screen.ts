255de70e239e175dd7ba51dcee49c173d13f5684
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-20 11:31:22 +0900

    Add read method to Screen

--- hardware/src/hardware/Screen.ts
+++ hardware/src/hardware/Screen.ts
@@ -1,41 +1,55 @@
 import { Binary, Binary16, Binary13, Binary12, Word } from './types';
 import { RAM4K } from './RAM4K';
 import { dmux } from './dmux';
-import { b, zero } from './helpers';
+import { b, b2s, zero } from './helpers';
+import { mux16 } from './mux16';
 
 export class Screen {
   private rams: [RAM4K, RAM4K] = [new RAM4K(), new RAM4K()];
 
-  public write(input: Binary16, load: Binary, address: Binary13): Word {
+  public write(input: Word, load: Binary, address: Binary13): Word {
+    if (load) {
+      console.log(`Screen write - input: ${b2s(input)}, address: ${b2s(address)}`);
+    }
     const bits = dmux(load, address[0]);
     const addrss = address.slice(1) as Binary12;
     this.rams[0].write(input, bits[0], addrss);
     this.rams[1].write(input, bits[1], addrss);
 
-    return /* TODO */ zero();
+    return input;
   }
 
-  public display() {
-    let len = 12;
-    const result = [];
-    for (const ram of this.rams) {
-      for (let i = 0; i < Math.pow(2, len); i += 1) {
-        const bi = (zero<number[]>(len).join('') + i.toString(2)).slice(len * -1);
-        const val = ram.read(b(bi));
-        result.push(val);
-      }
-    }
-    return result;
-
-    // const pixels = this.rams[0].read().concat(this.rams[1].read());
-    // let screen = '';
-    // for (let i = 0; i < pixels.length; i += 1) {
-    //   if (i !== 0 && i % 512 === 0) {
-    //     screen += '\n';
-    //   }
-    //   const pixel = pixels[i];
-    //   screen += pixel ? 'X' : ' ';
-    // }
-    // return screen;
+  public read(address: Binary13): Word {
+    return mux16(
+      this.rams[0].read([
+        address[1],
+        address[2],
+        address[3],
+        address[4],
+        address[5],
+        address[6],
+        address[7],
+        address[8],
+        address[9],
+        address[10],
+        address[11],
+        address[12],
+      ]),
+      this.rams[1].read([
+        address[1],
+        address[2],
+        address[3],
+        address[4],
+        address[5],
+        address[6],
+        address[7],
+        address[8],
+        address[9],
+        address[10],
+        address[11],
+        address[12],
+      ]),
+      address[0],
+    );
   }
 }
