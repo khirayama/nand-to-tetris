ab4541807d3700dd99dd8f8c00d0175eea282990
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-21 07:01:02 +0900

    Set resolution for screen

--- hardware/src/components/ScreenViewer.tsx
+++ hardware/src/components/ScreenViewer.tsx
@@ -1,35 +1,42 @@
 import * as React from 'react';
 import { css } from 'styled-jsx/css';
 
+import { Binary15 } from '../hardware/types';
+import { inc16 } from '../hardware/inc16';
+import { b, zero, b2s } from '../hardware/helpers';
+
 const styles = css`
   canvas {
+    width: 512px;
+    height: 256px;
     border: solid 2px #000;
   }
 `;
 
-export function ScreenViewer() {
+export function ScreenViewer(props: { memory: Memory }) {
   const ref = React.createRef<HTMLCanvasElement>();
   const width = 512;
-
-  const pixels: number[] = [];
-  for (let i = 0; i < 512 * 256; i += 1) {
-    pixels.push(i % 2 === 0 ? 0 : 1);
-  }
+  const res = 4;
 
   React.useEffect(() => {
     if (ref.current) {
       const ctx = ref.current.getContext('2d');
       if (ctx) {
+        const result = [];
+        let current = b('0100 0000 0000 0000');
+        const limit = Math.pow(2, 13);
+        for (let i = 0; i < limit; i += 1) {
+          result.push(props.memory.read(current.slice(1) as Binary15));
+          current = inc16(current);
+        }
+        const pixels = result.flat(2);
+
         for (let i = 0; i < pixels.length; i += 1) {
           const pixel = pixels[i];
           const l = Math.floor(i / width);
           const c = i % width;
-          ctx.fillRect(c, l, 1, 1);
-          if (pixel === 1) {
-            ctx.fillStyle = 'rgb(0, 0, 0)';
-          } else {
-            ctx.fillStyle = 'rgb(200, 200, 200)';
-          }
+          ctx.fillStyle = pixel === 1 ? 'rgba(0, 0, 0)' : 'rgb(233, 233, 233)';
+          ctx.fillRect(c * res, l * res, res, res);
           ctx.fill();
         }
       }
@@ -39,7 +46,7 @@ export function ScreenViewer() {
   return (
     <>
       <style jsx>{styles}</style>
-      <canvas width={512} height={256} ref={ref} />
+      <canvas width={width * res} height={(width / 2) * res} ref={ref} />
     </>
   );
 }
