55d281a040059420d1b4a761c5605b1e1e7185df
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-20 20:19:47 +0900

    Create RAMProxy

---
+++ hardware/src/helpers/RAMProxy.ts
@@ -0,0 +1,34 @@
+import { RAM16K } from '../hardware/RAM16K';
+import { Memory } from '../hardware/Memory';
+import { Binary, Binary15, Word } from '../hardware/types';
+import { b, zero } from '../hardware/helpers';
+
+export class RAMProxy {
+  private caches: { [key: string]: string } = {};
+
+  private ram: Memory | RAM16K;
+
+  constructor(ram: Memory | RAM16K) {
+    this.ram = ram;
+  }
+
+  public write(input: Word, load: Binary, address: Binary15) {
+    if (load) {
+      this.caches[address.join('')] = input.join('');
+    }
+    return this.ram.write(input, load, address);
+  }
+
+  public read(address: Binary15) {
+    const key = address.join('');
+    return this.caches[key] ? b(this.caches[key]) : zero();
+  }
+
+  public activeAddresses() {
+    return Object.keys(this.caches).map((addrss) => b(addrss));
+  }
+
+  public clear() {
+    this.caches = {};
+  }
+}
