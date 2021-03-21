13b3a40c2e94f9da01d420634fd5fab0e7d60c96
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-22 07:12:32 +0900

    Implement keyboard

--- hardware/src/hardware/Memory.ts
+++ hardware/src/hardware/Memory.ts
@@ -4,26 +4,22 @@ import { or } from './or';
 import { RAM16K } from './RAM16K';
 import { Screen } from './Screen';
 import { mux4way16 } from './mux4way16';
+import { Register } from './Register';
 
 export class Memory {
   private ram: RAM16K = new RAM16K();
 
   private screen: Screen = new Screen();
 
-  // TODO Keyboard
+  private keyboard: Register = new Register();
 
   public write(input: Word, load: Binary, address: Binary15) {
-    const [loadram0, loadram1, loadscreen] = dmux4way(load, [address[0], address[1]]);
+    const [loadram0, loadram1, loadscreen, loadkb] = dmux4way(load, [address[0], address[1]]);
     const loadram = or(loadram0, loadram1);
     const ramout = this.ram.write(input, loadram, address);
     const scrout = this.screen.write(input, loadscreen, address.slice(2) as Binary13);
-    return mux4way16(
-      ramout,
-      ramout,
-      scrout,
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] /* keyboardout */,
-      [address[0], address[1]],
-    );
+    const kbout = this.keyboard.write(input, loadkb);
+    return mux4way16(ramout, ramout, scrout, kbout, [address[0], address[1]]);
   }
 
   public read(address: Binary15) {
@@ -31,7 +27,7 @@ export class Memory {
       this.ram.read(address),
       this.ram.read(address),
       this.screen.read(address.slice(2) as Binary13),
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] /* keyboardout */,
+      this.keyboard.read(),
       [address[0], address[1]],
     );
   }
