6b60a9a8d3c4245db3ec15e08536daa184b07ef7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-22 07:12:53 +0900

    Update RAMProxy

--- hardware/src/helpers/RAMProxy.ts
+++ hardware/src/helpers/RAMProxy.ts
@@ -4,7 +4,7 @@ import { Binary, Binary15, Word } from '../hardware/types';
 import { b, zero } from '../hardware/helpers';
 
 export class RAMProxy {
-  private caches: { [key: string]: string } = {};
+  private caches: { [key: string]: Word } = {};
 
   private ram: Memory | RAM16K;
 
@@ -14,17 +14,18 @@ export class RAMProxy {
 
   public write(input: Word, load: Binary, address: Binary15) {
     if (load === 1) {
-      this.caches[address.join('')] = input;
+      const key = address.join('');
+      this.caches[key] = input;
     }
     return this.ram.write(input, load, address);
   }
 
-  public read(address: Binary15) {
+  public read(address: Binary15): Word {
     const key = address.join('');
     return this.caches[key] ? this.caches[key] : zero();
   }
 
-  public activeAddresses() {
+  public activeAddresses(): Binary15[] {
     return Object.keys(this.caches)
       .sort()
       .map((addrss) => b(addrss));
