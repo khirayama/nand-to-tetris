a4a5ff378cbd4ec540f9669e20485a5c601dbd0d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-03-26 07:40:24 +0900

    Rename RAMProxy to RAMMock

--- hardware/src/helpers/RAMMock.ts
+++ hardware/src/helpers/RAMMock.ts
@@ -1,23 +1,14 @@
-import { RAM16K } from '../hardware/RAM16K';
-import { Memory } from '../hardware/Memory';
 import { Binary, Binary15, Word } from '../hardware/types';
 import { b, zero } from '../hardware/helpers';
 
-export class RAMProxy {
+export class RAMMock {
   private caches: { [key: string]: Word } = {};
-
-  private ram: Memory | RAM16K;
-
-  constructor(ram: Memory | RAM16K) {
-    this.ram = ram;
-  }
 
   public write(input: Word, load: Binary, address: Binary15) {
     if (load === 1) {
       const key = address.join('');
       this.caches[key] = input;
     }
-    return this.ram.write(input, load, address);
   }
 
   public read(address: Binary15): Word {
