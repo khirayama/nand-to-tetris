165850efd8d0eb30352c10ae0aa1469b4efb1a1b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-20 07:46:58 +0900

    Add methods of Parser

--- src/software/assembler/Parser.ts
+++ src/software/assembler/Parser.ts
@@ -1,1 +1,36 @@
-export class Parser {}
+const CommandType = {
+  A_COMMAND: 'A_COMMAND',
+  C_COMMAND: 'C_COMMAND',
+  L_COMMAND: 'L_COMMAND',
+} as const;
+type CommandType = typeof CommandType[keyof typeof CommandType];
+
+export class Parser {
+  constructor() {}
+
+  private hasMoreCommands(): boolean {
+    return true;
+  }
+
+  private advance(): void {}
+
+  private commandType(): CommandType {
+    return CommandType.A_COMMAND;
+  }
+
+  private symbol(): string {
+    return '';
+  }
+
+  private dest(): string {
+    return '';
+  }
+
+  private comp(): string {
+    return '';
+  }
+
+  private jump(): string {
+    return '';
+  }
+}
