038d270a8c6e353de0967b0a6bfd7726c1acbce9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-21 07:58:13 +0900

    Add methods to Code

--- src/software/assembler/Code.ts
+++ src/software/assembler/Code.ts
@@ -1,2 +1,9 @@
+import { Binary3, Binary7 } from '../../hardware/types';
+
 export class Code {
+  private dest(mnemonic: string): Binary3 {}
+
+  private comp(mnemonic: string): Binary7 {}
+
+  private jump(mnemonic: string): Binary3 {}
 }
