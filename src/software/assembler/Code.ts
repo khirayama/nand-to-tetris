27d99ac985e699218861ade04f63b91db8333c5b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-22 07:04:09 +0900

    Return temporary values

--- src/software/assembler/Code.ts
+++ src/software/assembler/Code.ts
@@ -1,9 +1,15 @@
 import { Binary3, Binary7 } from '../../hardware/types';
 
 export class Code {
-  private dest(mnemonic: string): Binary3 {}
+  private dest(mnemonic: string): Binary3 {
+    return [0, 0, 0];
+  }
 
-  private comp(mnemonic: string): Binary7 {}
+  private comp(mnemonic: string): Binary7 {
+    return [0, 0, 0, 0, 0, 0, 0];
+  }
 
-  private jump(mnemonic: string): Binary3 {}
+  private jump(mnemonic: string): Binary3 {
+    return [0, 0, 0];
+  }
 }
