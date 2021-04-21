78a18f005b827bdc7a2752d26562d03b9fe6879f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-22 07:07:53 +0900

    Create SymbolTable class

---
+++ src/software/assembler/SymbolTable.ts
@@ -0,0 +1,9 @@
+export class SymbolTable {
+  constructor() {}
+
+  private addEntry(sym: string, address: number): void {}
+
+  private contains(sym: string): boolean {}
+
+  private getAddress(sym: string): number {}
+}
