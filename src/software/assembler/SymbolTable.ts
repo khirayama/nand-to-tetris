fa364b3f786f77f9bd9ae22c9e1644ad22ff2ba8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-23 07:20:54 +0900

    SymbolTable methods return temporary value

--- src/software/assembler/SymbolTable.ts
+++ src/software/assembler/SymbolTable.ts
@@ -3,7 +3,11 @@ export class SymbolTable {
 
   private addEntry(sym: string, address: number): void {}
 
-  private contains(sym: string): boolean {}
+  private contains(sym: string): boolean {
+    return true;
+  }
 
-  private getAddress(sym: string): number {}
+  private getAddress(sym: string): number {
+    return 0;
+  }
 }
