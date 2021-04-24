d7ffe58452d24bcdc11ac356e593097d33b550fa
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-25 07:08:35 +0900

    Add test items

--- src/software/assembler/Parser.spec.ts
+++ src/software/assembler/Parser.spec.ts
@@ -2,6 +2,10 @@ import assert from 'power-assert';
 import { Parser } from './Parser';
 
 describe('Parser', function () {
-  it('', function () {
-  });
+  it('hasMoreCommands', function () {});
+  it('advance', function () {});
+  it('symbol', function () {});
+  it('dest', function () {});
+  it('comp', function () {});
+  it('jump', function () {});
 });
