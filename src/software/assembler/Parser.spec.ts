59deb4fe68d53e5d26c72db4170f075de3e6f824
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-27 07:29:57 +0900

    Add test case to Parser.hasMoreCommands

--- src/software/assembler/Parser.spec.ts
+++ src/software/assembler/Parser.spec.ts
@@ -2,7 +2,12 @@ import assert from 'power-assert';
 import { Parser } from './Parser';
 
 describe('Parser', function () {
-  it('hasMoreCommands', function () {});
+  it('hasMoreCommands', function () {
+    let parser = new Parser(['', '']);
+    assert.equal(parser.hasMoreCommands(), true);
+    parser = new Parser([]);
+    assert.equal(parser.hasMoreCommands(), false);
+  });
   it('advance', function () {});
   it('symbol', function () {});
   it('dest', function () {});
