1fad72f7caf419cc34f0e87773595f2469b0e2cf
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-04-29 11:55:05 +0900

    Implement whole flow of assembler

--- src/software/assembler/index.ts
+++ src/software/assembler/index.ts
@@ -1,9 +1,29 @@
-import { Parser } from './Parser';
+import { Parser, CommandType } from './Parser';
 import { Code } from './Code';
 import { SymbolTable } from './SymbolTable';
 
 export function assembler() {
-  const parser = new Parser();
+  const parser = new Parser([]);
   const code = new Code();
   const symbolTable = new SymbolTable();
+
+  let ROMAddress = 0;
+  const RAMAddress = 16;
+
+  while (parser.hasMoreCommands()) {
+    const commandType = parser.commandType();
+    if (commandType == CommandType.A_COMMAND || commandType === CommandType.C_COMMAND) {
+      ROMAddress = ROMAddress + 1;
+    } else if (commandType === CommandType.L_COMMAND) {
+      const sym = parser.symbol();
+
+      if (!symbolTable.constants(sym)) {
+        const address = '0x' + ('0000' + ROMAddress.toString(16).slice(-4));
+        symbolTable.addEntry(sym, address);
+      }
+    } else {
+      throw new Error('Invalid Command Type');
+    }
+    parser.advance();
+  }
 }
