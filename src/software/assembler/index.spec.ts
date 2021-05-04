1ec1dcdcc36c3dc246dc3466fdac0de8c93822fe
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-05-04 14:38:01 +0900

    Add testcase to assembler

--- src/software/assembler/index.spec.ts
+++ src/software/assembler/index.spec.ts
@@ -3,6 +3,18 @@ import { assembler } from './';
 
 describe('assembler', function() {
   it('Run correctly' function() {
-    assembler([]);
+    const addASM = ['@2', 'D=A', '@3', 'D=D+A', '@0', 'M=D'];
+
+    const addHack = [
+      '0000000000000010',
+      '1110110000010000',
+      '0000000000000011',
+      '1110000010010000',
+      '0000000000000000',
+      '1110001100001000',
+    ];
+
+    const result = assembler(addASM);
+    assert.deepEqual(result, addHack);
   });
 });
